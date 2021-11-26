2f862ba5cae54a8ccaa2f08db814642506487126
Iwan Dwi Prakoso <iwan.dprakoso@gmail.com>
2021-11-26 07:02:31 +0700

    feat: add leaderboard

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css';
 
-import * as THREE from 'three';
+import * as THREE from "three";
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { Water } from 'three/examples/jsm/objects/Water.js';
